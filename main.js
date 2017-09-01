2ae31b28d22a5ae8b1096b8340621d29d9ba3939
Arthur <arthur.sw@gmail.com>
2017-09-01 10:44:44 +0200

    added gui

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
 var canvas = document.getElementById('canvas');
 paper.setup(canvas);
 
-let MaxDistance = 1
+let parameters = {
+	simplify: 2.5,
+	straightLines: true,
+	maxAngle: 5,
+	select: false
+};
 
 let path = null;
 
@@ -12,13 +17,25 @@ let tool = new paper.Tool();
 newPath = function(continuePath=false) {
 	let lastPoint = null
 	if(path != null) {
-		path.simplify(10)
+		path.simplify(parameters.simplify)
+		if(parameters.straightLines && path.segments.length > 1) {
+			for(let segment of path.segments) {
+				let curve = segment.curve
+				if(curve != null && curve.handle1 != null && curve.handle2 != null && Math.abs(Math.abs(curve.handle1.getDirectedAngle(curve.handle2)) - 180) < parameters.maxAngle) {
+					curve.handle1 = null
+					curve.handle2 = null
+				}
+			}
+		}
 		if(path.lastSegment != null) {
 			lastPoint = path.lastSegment.point
 		}
-		path.fullySelected = true
+		if(parameters.select) {
+			path.fullySelected = true
+		}
 	}
 	path = new paper.Path();
+
 	path.strokeColor = 'black';
 	path.strokeWidth = 5;
 	path.strokeCap = 'round';
@@ -51,3 +68,10 @@ window.onresize = function (event) {
 	paper.view.viewSize.width = window.innerWidth;
 	paper.view.viewSize.height = window.innerHeight;
 }
+
+var gui = new dat.GUI()
+
+gui.add(parameters, 'simplify', 0, 100).name('Simplify amount')
+gui.add(parameters, 'straightLines').name('Straight lines')
+gui.add(parameters, 'maxAngle', 0, 90).name('Max angle')
+gui.add(parameters, 'select').name('Select')
